74682ebfe0e4d1124c2d73d24bd7e737dc222f88
santisa147 <santisa147@gmail.com>
2026-04-19 23:45:48 -0300

    UpdateWFunctions & SaveData

--- Project CLI task manager/tasks.py
+++ Project CLI task manager/tasks.py
@@ -1,6 +1,50 @@
-tasks = []
 
+def add_tasks(tasks):
+    task = {'title': input("Task Name: "), 'done' : False}
+    if task not in tasks:
+            tasks.append(task)
+    else: print('The Task is already added')
+
+def view_tasks(tasks):
+    for i, task in enumerate(tasks, start=1):
+        status = "✅" if task["done"] else "❌"
+        print(f"{i}. {task['title']} {status}")
+
+def mark_done(tasks):
+    a = 0
+    view_tasks(tasks)
+    a = int(input('Select the Task to Mark Done: ')) - 1
+    tasks[a]['done']= True if tasks[a]['done'] == False  else print('The task is already Done')
+    
+def delete_task(tasks):
+    a = 0
+    view_tasks(tasks)
+    a = int(input('Select the Task to Delete: ')) - 1
+    tasks.pop(a)
+    
+def save_tasks(tasks):
+    with open("tasks.txt", "w") as file:
+        for task in tasks:
+            line = f"{task['title']}|{task['done']}\n"
+            file.write(line)
+
+def load_tasks():
+    tasks = []
+    try:
+        with open("tasks.txt", "r") as file:
+            for line in file:
+                title, done = line.strip().split("|")
+                tasks.append({
+                    "title": title,
+                    "done": done == "True"
+                })
+    except FileNotFoundError:
+        pass
+    return tasks
+
+tasks = load_tasks()
 while True:
+   
     print("1. Add task")
     print("2. View tasks")
     print("3. Mark task as done")
@@ -9,28 +53,17 @@ while True:
 
     choice = str(input("Choose an option: "))
     if choice == "1":
-        task = {'title': input("Task Name: "), 'done' : False}
-        if task not in tasks:
-            tasks.append(task)
-        else: print('The Task is already added')
+        add_tasks(tasks)
     elif choice == '2':
-        print(tasks)
+       view_tasks(tasks)
     elif choice == '3':
-        a=0
-        for i in tasks:
-            a=a+1
-            print(a, i)
-        a = int(input('Select the Task to mark done: ')) - 1
-        if tasks[a]['done'] == False:
-            tasks[a]['done']= True
-        else: print('The task is already Done')
+        mark_done(tasks)
     elif choice == '4':
-        a=0
-        for t in tasks:
-            a=a+1
-            print(a, t)
-        a = int(input('Select the Task to Delete: ')) - 1
-        tasks.pop(a)
+        delete_task(tasks)
     elif choice == '5':
+        save_tasks(tasks)
         break
     else:print('Select a correct option')
+
+
+        
